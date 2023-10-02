8b7579e3aecde79831821537b5c3b2ed67c91cf5
Zea-Zee <g.kuznetsov2@g.nsu.ru>
2023-10-02 23:57:12 +0700

    3+4 HWs

--- PHC/lab4.py
+++ PHC/lab4.py
@@ -8,15 +8,15 @@ def shuffle_data():
     argparser.add_argument("input_2")
     argparser.add_argument("P")
     args = argparser.parse_args()
-    input_1 = str(args.input_1)
-    input_2 = str(args.input_2)
     P = str(args.P)
     real_data_istream = open(args.input_1, 'r')
     synthetic_data_istream = open(args.input_2, 'r')
     real_data = (str(real_data_istream.readline())).split(' ')
     synthetic_data = (str(synthetic_data_istream.readline())).split(' ')
     print("Args:", real_data, synthetic_data, "P:", P)
-    selecter = np.random.choice(5, len(real_data))
-    return np.where(selecter < 1 / float(P) - 1, real_data, synthetic_data)
+    selector = np.random.choice(5, len(real_data))
+    return np.where(selector < 1 / float(P) - 1, real_data, synthetic_data) if float(P) > 0 else real_data
+
 
 print("Res:", shuffle_data())
+#   python lab4.py random_select_1.txt random_select_2.txt 0.2
