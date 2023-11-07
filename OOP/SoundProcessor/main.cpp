5313b32ef5f0baab1d836c24d9d4c3b76a9af60a
Zea-Zee <g.kuznetsov2@g.nsu.ru>
2023-11-07 10:30:37 +0700

    muter

--- OOP/SoundProcessor/main.cpp
+++ OOP/SoundProcessor/main.cpp
@@ -2,11 +2,11 @@
 #include <string>
 #include <fstream>
 #include <cstdint>
+#include <cstring>
 
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 1024
 
 using namespace std;
-
 
 
 class BaseError{
@@ -65,8 +65,9 @@ class WavFile {
 private:
     wav_hdr *header;
     ifstream wavFile;
+    char *path;
 public:
-    WavFile(const char *path) {
+    WavFile(char *path) : path{path}{
         wavFile.open(path, std::ios::binary);
         cout << "BEBRA";
         if (not wavFile) {
@@ -78,13 +79,15 @@ public:
         wavFile.read(reinterpret_cast<char*>(header), sizeof(*header));
         std::cout << "Header Read " << wavFile.gcount() << " bytes." << std::endl;
         printHeader();
-//
+        cout << "Subchunk2Size is " << header->Subchunk2Size << endl;
+        if(header->Subchunk2Size <= header->ChunkSize / 2) header->Subchunk2Size = header->ChunkSize - 36;
+        cout << "Subchunk2Size is " << header->Subchunk2Size << endl;
         if (wavFile.gcount() > 0){
             if(header->AudioFormat != 1) WAVFormatError err(3, "Audio format must be PMC (without encoding) (field must have val 1)");
             if(header->NumOfChan != 1) WAVFormatError err(4, "WAV for this program must have one channel (Mono) (field must have val 1)");
             if(header->bitsPerSample != 16) WAVFormatError err(5, "WAV for this program must have 16 bitness (field must have val 16)");
             if(header->SamplesPerSec != 44100) WAVFormatError err(6, "WAV for this program can only have 44100 sampling rate (field must have val 44100)");
-
+        wavFile.close();
 //            uint16_t bytesPerSample = header->bitsPerSample / 8; // Number of bytes per sample
 //            uint32_t numSamples = header->Subchunk2Size / bytesPerSample;
 //            std::cout << numSamples << " samples in WAV file" << std::endl;
@@ -97,26 +100,6 @@ public:
 //                }
 //                std::cout << "Sample " << i << ": " << sample << std::endl;
 //            }
-            const int muteDurationInSeconds = 60;
-            ofstream outputFile("ex1o.wav", std::ios::binary);
-
-            const uint32_t bytesToMute = header->SamplesPerSec * header->NumOfChan * (header->bitsPerSample / 8) * muteDurationInSeconds;
-            const uint32_t bufferSize = 1024; // Adjust buffer size as needed
-            char silenceBuffer[bufferSize] = {0};
-
-            // Mute the first minute by writing silence to the output file
-            for (uint32_t i = 0; i < bytesToMute; i += bufferSize) {
-                const uint32_t bytesToWrite = std::min(bufferSize, bytesToMute - i);
-                outputFile.write(silenceBuffer, bytesToWrite);
-            }
-
-            // Copy the remaining audio data
-            outputFile << wavFile.rdbuf();
-
-            wavFile.close();
-            outputFile.close();
-
-            std::cout << "Muted the first " << muteDurationInSeconds << " seconds and saved to " << std::endl;
 
 
         }else{
@@ -124,6 +107,16 @@ public:
 //            perror("Empty wav header, try again with another file");
 //            exit(2);
         }
+    }
+    WAV_HEADER * getHeader(){
+        WAV_HEADER *wav_header_copy = this->header;
+        return wav_header_copy;
+    }
+    char *getPath(){
+        char *cpy = (char*) calloc(strlen(path) + 1, 1);
+        strcpy(cpy, path);
+        cout << strlen(path) << ":" << cpy << ":" << path << endl;
+        return cpy;
     }
     void printHeader(){
         cout << "RIFF header                :" << header->RIFF[0] << header->RIFF[1] << header->RIFF[2] << header->RIFF[3] << endl;
@@ -138,7 +131,9 @@ public:
         cout << "Number of bytes per second :" << header->bytesPerSec << endl;
         cout << "Data length                :" << header->Subchunk2Size << endl;
         cout << "Audio Format               :" << header->AudioFormat << endl;
+        cout << "ChunkSize1                 :" << header->Subchunk1Size << endl;
         // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+
 
         cout << "Block align                :" << header->blockAlign << endl;
         cout << "Data string                :" << header->Subchunk2ID[0] << header->Subchunk2ID[1] << header->Subchunk2ID[2] << header->Subchunk2ID[3] << endl;
@@ -147,55 +142,14 @@ public:
 
 
 
-//class Converter{
-//private:
-//    unsigned int start_time;
-//    unsigned int end_time;
-//public:
-//    Converter(unsigned int start, unsigned int end):start_time{start}, end_time{end}{}
-//};
-//
-//class WAVMuter : public Converter{
-//};
-//
-//class WAVMixer : public Converter{
-//};
-//
-//class  WAVAccelerator: public Converter{
-//};
-//
-//class ConverterFactory {
-//public:
-//    virtual Converter *createConverter(){};
-//    virtual ~ConverterFactory(){};
-//};
-//
-//class WAVMuterFactory : public ConverterFactory{
-//public:
-//    Converter *createConverter() override{
-//        return new WAVMuter();
-//    }
-//};
-//
-//class WAVAcceleratorFactory : public ConverterFactory{
-//    Converter *createConverter() override{
-//        return new WAVAccelerator();
-//    }
-//};
-//
-//class WAVMixerFactory : public ConverterFactory{
-//    Converter *createConverter() override{
-//        return new WAVMixer();
-//    }
-//};
 class WAVConverter {
-private:
+protected:
     unsigned int start_time;
     unsigned int end_time;
-    const char *path;
-    unsigned int header_length;
+    char *path;
+    WavFile *wav;
 public:
-    WAVConverter(unsigned int start, unsigned int end, const char *p, unsigned int header_len) : start_time{start}, end_time{end}, path{p}, header_length{header_len}{
+    WAVConverter(unsigned int start, unsigned int end, char *p, WavFile *wav) : start_time{start}, end_time{end}, path{p}, wav{wav}{
         convert();
     }
     virtual void convert() {};
@@ -204,15 +158,75 @@ public:
 
 class WAVMuter : public WAVConverter {
 public:
-    WAVMuter(unsigned int start, unsigned int end, const char *p, unsigned int header_len) : WAVConverter(start, end, p, header_len){};
+    WAVMuter(unsigned int start, unsigned int end, char *p, WavFile *wav) : WAVConverter(start, end, p, wav){};
     void convert() override{
-        std::cout << "Converting WAV to MP3" << std::endl;
+        if(start_time > end_time) MyWarning a(127, "Start time of mute can't be bigger then end time.");
+        ifstream input(wav->getPath(), std::ios::binary);
+        ofstream output("tmp.wav", std::ios::binary);
+        cout << *wav->getPath() << endl;
+
+        // Calculate the duration of the audio in seconds
+//        double duration = static_cast<double>(wav->getHeader()->Subchunk2Size) / wav->getHeader()->bytesPerSec;
+        double duration = static_cast<double>(wav->getHeader()->Subchunk2Size) / (wav->getHeader()->bitsPerSample / 8) / wav->getHeader()->NumOfChan / wav->getHeader()->SamplesPerSec;
+
+        if (!output) {
+            std::cerr << "Error: Unable to open output file " << std::endl;
+            exit(127);
+        }
+
+        if (!input) {
+            std::cerr << "Error: Unable to open input file " << std::endl;
+            exit(127);
+        }
+
+        // Write the WAV header to the output file
+        input.read(reinterpret_cast<char*>(wav->getHeader()), sizeof(*wav->getHeader()));
+        output.write(reinterpret_cast<char*>(wav->getHeader()), sizeof(*wav->getHeader()));
+
+        uint32_t startSample = static_cast<uint32_t>(start_time * wav->getHeader()->SamplesPerSec);
+        uint32_t endSample = static_cast<uint32_t>(end_time * wav->getHeader()->SamplesPerSec);
+//        cout << "startSample " << startSample << "\n";
+//        cout << "endSample " << endSample << "\n";
+
+        // Mute the specified range
+        char buffer[BUFFER_SIZE];
+        double currentTime = 0.0;
+//        int numOfSamples = wav->getHeader()->Subchunk2Size / (wav->getHeader()->bitsPerSample / 8) / wav->getHeader()->NumOfChan;
+        int numOfSamples = (int) wav->getHeader()->Subchunk1Size / 2;
+//        int numOfSamples = (int) wav->getHeader()->Subchunk2Size / 2;
+        cout << "Number of samples is: " << numOfSamples << "\n";
+
+        for (uint32_t sampleIndex = 0; sampleIndex < numOfSamples; sampleIndex++) {
+            int16_t sample;
+            input.read(reinterpret_cast<char*>(&sample), sizeof(int16_t));
+
+            if (sampleIndex >= startSample && sampleIndex < endSample) {
+                sample = 0;  // Set the sample to zero to mute it
+            }
+
+            // Write the modified sample to the output file
+            output.write(reinterpret_cast<char*>(&sample), sizeof(int16_t));
+//            cout << "Sample've read with idx: " << sampleIndex << " " << "sample is " << sample << "\n";
+        }
+        int n = 0;
+//        while (!input.eof()) {
+//            n++;
+//            int16_t sample;
+//            input.read(reinterpret_cast<char*>(&sample), sizeof(int16_t));
+//            output.write(reinterpret_cast<char*>(&sample), sizeof(int16_t));
+//        }
+        cout << wav->getHeader()->NumOfChan << " " << wav->getHeader()->Subchunk2Size << " " << wav->getHeader()->ChunkSize << " " << wav->getHeader()->Subchunk1Size << "\n";// << "" << wav->getHeader()->NumOfChan << "" << wav->getHeader()->NumOfChan << ""
+        cout << "n is " << n << "\n";
+        input.close();
+        output.close();
+
+        std::cout << "Muted audio from " << start_time << " to " << end_time << " seconds." << std::endl;
     }
 };
 
 class WAVMixer : public WAVConverter {
 public:
-    WAVMixer(unsigned int start, unsigned int end, const char *p, unsigned int header_len) : WAVConverter(start, end, p, header_len){};
+    WAVMixer(unsigned int start, unsigned int end, char *p, WavFile *wav) : WAVConverter(start, end, p, wav){};
     void convert() override{
         std::cout << "Converting WAV to MP3" << std::endl;
     }
@@ -220,7 +234,7 @@ public:
 
 class WAVAccelerator : public WAVConverter {
 public:
-    WAVAccelerator(unsigned int start, unsigned int end, const char *p, unsigned int header_len) : WAVConverter(start, end, p, header_len){};
+    WAVAccelerator(unsigned int start, unsigned int end, char *p, WavFile *wav) : WAVConverter(start, end, p, wav){};
     void convert() override{
         std::cout << "Converting WAV to MP3" << std::endl;
     }
@@ -228,13 +242,13 @@ public:
 
 class WAVConverterFactory {
 public:
-    static WAVConverter* createConverter(const std::string& type, unsigned int start, unsigned int end, const char *p, unsigned int header_len) {
+    static WAVConverter* createConverter(const std::string& type, unsigned int start, unsigned int end, char *p, WavFile *wav) {
         if (type == "Mute") {
-            return new WAVMuter(start, end, p, header_len);
+            return new WAVMuter(start, end, p, wav);
         } else if (type == "Mix") {
-            return new WAVMixer(start, end, p, header_len);
+            return new WAVMixer(start, end, p, wav);
         } else if (type == "Accelerate") {
-            return new WAVAccelerator(start, end, p, header_len);
+            return new WAVAccelerator(start, end, p, wav);
         } else {
             return nullptr; // Invalid type
         }
@@ -242,12 +256,10 @@ public:
 };
 
 int main() {
-    // Usage example
-    std::string conversionType = "MP3";
-    std::string inputFile = "input.wav";
-    std::string outputFile = "output.mp3";
-
-    WAVConverter* converter = WAVConverterFactory::createConverter("Mute", 0, 5, "bebra", 44);
+    WavFile input("./ex1.wav");
+//    WavFile tmp("./ex1.wav");
+    WAVConverter* converter = WAVConverterFactory::createConverter("Mute", 10, 30, "bebra", &input);
+    converter->convert();
 
 //    if (converter) {
 //        converter->convert(inputFile, outputFile);
