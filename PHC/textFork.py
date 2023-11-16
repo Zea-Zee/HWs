f2bdcc2534db94c5fd5bf891b37c8c248dcdcbee
Zea-Zee <g.kuznetsov2@g.nsu.ru>
2023-11-16 22:18:21 +0700

    3rd version of text recognizer with Q, I, 1 and others

--- PHC/textFork.py
+++ PHC/textFork.py
@@ -3,13 +3,24 @@ import numpy as np
 import requests
 from PIL import Image
 
-HEIGHT = 54
+# for 0.8 koef works correctly
+# for 0.85 NOT
+skip_koeff = 0.8
+HEIGHT = 200
+waitFlag = 0
 
-def wait():
-    key = cv2.waitKey(0)
-    if key == ord('q'):
-        exit(0)
-    cv2.destroyAllWindows()
+def wait(w=0):
+    if waitFlag or w:
+        key = cv2.waitKey(0)
+        if key == ord('q'):
+            exit(0)
+        cv2.destroyAllWindows()
+
+def sh(window_name, img, w=1):
+    if waitFlag or w == 2:
+        cv2.imshow(window_name, img)
+        if w > 0:
+            wait(1)
 
 def cut_corners(img):
     color_converted = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
@@ -22,73 +33,6 @@ def cut_corners(img):
     ret = np.array(im)
     ret = cv2.cvtColor(ret, cv2.COLOR_BGR2GRAY)
     return ret
-
-def match_letter(letter, alphabet):
-    # scores = {}
-    # for char in range(ord('A'), ord('Z') + 1):
-    #     scores[char] = 0
-    #     # scores.append(0)
-    # for char in range(ord('1'), ord('9') + 1):
-    #     scores[char] = 0
-    #     # scores.append(0)
-    # # print(scores)
-    # cv2.imshow("let", alphabet[0])
-    # wait()
-    biggest_index = 0
-    biggest_val = 0
-
-    for i in range(len(alphabet)):
-        feat = cv2.ORB_create()
-        kp1, des1 = feat.detectAndCompute(alphabet[i], None)
-        kp2, des2 = feat.detectAndCompute(letter, None)
-
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = bf.knnMatch(des1, des2)
-        count = 0
-        matched_image = cv2.drawMatchesKnn(alphabet[i],kp1, letter, kp2, matches, None,matchColor=(0, 255, 0), matchesMask=None,singlePointColor=(255, 0, 0), flags=0)
-        # cv2.imshow("matches", matched_image)
-        # wait()
-        for m, n in matches:
-            if m.distance < 0.9 * n.distance:
-                count += 1
-        if count > biggest_index:
-            biggest_index = count
-            biggest_val = i
-    if biggest_index == 0:
-        biggest_index = 78
-    elif biggest_index < 13:
-        biggest_index += 66
-    elif biggest_index >= 13 and biggest_index < 26:
-        biggest_index += 65
-    else:
-        biggest_index += 23
-    return chr(biggest_index)
-
-
-def match_letter_templater(letter, alphabet):
-    biggest_index = 0
-    biggest_val = 0
-    lh, lw = letter.shape
-    letter = cv2.resize(letter, (int(lw / lh * HEIGHT), HEIGHT))
-
-    for i in range(len(alphabet)):
-        ah, aw = alphabet[i].shape
-        alphabet[i] = cv2.resize(alphabet[i], (int(aw / ah * HEIGHT), HEIGHT))
-        res = cv2.matchTemplate(alphabet[i], letter, cv2.TM_CCOEFF_NORMED)
-        minval, maxval, minloc, maxloc = cv2.minMaxLoc(res)
-        # print(maxval)
-        if maxval > biggest_val:
-            biggest_index = i
-            biggest_val = maxval
-    if biggest_index == 0:
-        biggest_index = 78
-    elif biggest_index <= 13:
-        biggest_index += 64
-    elif biggest_index > 13 and biggest_index < 26:
-        biggest_index += 65
-    else:
-        biggest_index += 23
-    return chr(biggest_index)
 
 
 def sort_by_text(lst, img):
@@ -105,144 +49,169 @@ def sort_by_text(lst, img):
         positions.append(max_loc)
         # cv2.imshow("img", img)
         # cv2.imshow("let", letter_image)
-        # wait()
+        #
         # print(lst)
 
     sorted_letter_images = [image for _, image in sorted(zip(positions, lst), key=lambda x: x[0])]
+    for i in range(len(sorted_letter_images)):
+        h, w = sorted_letter_images[i].shape
+        sorted_letter_images[i] = letter = cv2.resize(sorted_letter_images[i], (int(w / h * HEIGHT) - 1, int(HEIGHT) - 1))
     return sorted_letter_images
 
 def get_letters(templates):
     letters = []
-    # th, tw = templates.shape
-    # templates = cv2.resize(templates, (tw * 10, th * 10))
     _, mask = cv2.threshold(templates, 150, 255, cv2.THRESH_BINARY_INV)
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
-        # template_contour = np.zeros_like(templates)
-        # cv2.drawContours(template_contour, contour, -1, (255, 0, 0), 4)
-        # cv2.imshow("
-        # Cont", template_contour)
-        # wait()
         x, y, w, h = cv2.boundingRect(contour)
         letter = templates[y:y + h, x:x + w]
-        h, w = letter.shape
-        # if w / h >= 2:
-        #     double_letter = cv2.resize(letter, (w * 20, h * 20))
-        #     _, new_mask = cv2.threshold(double_letter, 150, 255, cv2.THRESH_BINARY_INV)
-        #     new_contours, _ = cv2.findContours(new_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        #     x, y, w, h = cv2.boundingRect(new_contours[0])
-        #
-        #     letter = double_letter[y:y + h, x:x + w]
-        #     letter = cv2.resize(letter, (w, h))                         #N
-        #     cv2.imshow("Let", letter)
-        #     wait()
-        #     letters.append(letter)
-        #
-        #     x, y, w, h = cv2.boundingRect(new_contours[1])
-        #     letter = double_letter[y:y + h, x:x + w]
-        #     letter = cv2.resize(letter, (w, h))                         #M
-        # letter = cv2.resize(letter, (int(w / h * HEIGHT), HEIGHT))
+        # print(letter.shape)
         letters.append(letter)
-        # cv2.imshow("Let", letter)
-        # wait()
-        # cv2.destroyAllWindows()
-
     return letters
 
-def get_text_contours(img):
-    _, mask = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY_INV)
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contour_image = np.zeros_like(img)
-    cv2.drawContours(contour_image, contours, -1, (255, 0, 0), 4)
-    # cv2.imshow('Mask', mask)
-    # cv2.imshow('Contours', contour_image)
-    # wait()
-
-    # for i in range(len(contours)):
-    #     cv2.drawContours(contour_image, contours[i], -1, (255, 0, 0), 4)
-    #     # cv2.imshow('Mask', mask)
-    #     cv2.imshow('Contours', contour_image)
-    #     # cv2.waitKey(200)
-    #     wait()
-    return contour_image
-
 def get_letter(img, alphabet):
+# def get_letter(img, alphabet, used_width):
     biggest_val = float('-inf')
     biggest_index = 0
     biggest_pos = 0
     h, w = 0, 0
     for i in range(len(alphabet)):
+        # print(img.shape, template.shape)
         res = cv2.matchTemplate(img, alphabet[i], cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
         # print(maxval)
         th, tw = alphabet[i].shape
-        if max_val > biggest_val or (max_val + 0.2 > biggest_val and tw > w):
+        # if(i == 8):
+        #     print(biggest_val, max_val, w, tw)
+        #     sh("let", img, 0)
+        #     sh("max let", alphabet[i])
+        # if (max_val > biggest_val * 0.8 and tw > w) or (max_val > biggest_val * 1.2):
+        # if tw < HEIGHT / 2 and tw > HEIGHT / 3:
+        #     print(tw, HEIGHT, HEIGHT / tw)
+        #     sh("narrow", alphabet[i], 2)
+        is1 = tw > HEIGHT / 2.8 and tw < HEIGHT / 2.7
+        isI = tw > HEIGHT / 2.5 and tw < HEIGHT / 2.4
+        isJ = tw > HEIGHT / 1.7 and tw < HEIGHT / 1.9
+        if max_val > skip_koeff and max_val > biggest_val and (tw > HEIGHT / 2) or ((is1 or isI) and max_val > 0.9 and max_val > biggest_val):
             biggest_index = i
+            # print(biggest_val, max_val, w, tw)
             biggest_val = max_val
             biggest_pos = max_loc
             h, w = alphabet[i].shape
-            cv2.imshow("let", img)
-            cv2.imshow("max let", alphabet[i])
-            wait()
+            sh("let", img, 0)
+            sh("max let", alphabet[i])
+
     if biggest_index == 0:
         biggest_index = 78
     elif biggest_index <= 13:
         biggest_index += 64
-    elif biggest_index > 13 and biggest_index < 26:
+    elif biggest_index > 15 and biggest_index < 25:
+        biggest_index += 66
+    elif biggest_index > 13 and biggest_index < 25:
         biggest_index += 65
     else:
-        biggest_index += 23
-    if biggest_val < 0.5:
-        return -1
-    print(biggest_val, biggest_pos, biggest_index, w, h)
-    cv2.rectangle(img, biggest_pos, (biggest_pos[0] + w, biggest_pos[1] + h), (0, 0, 255), -1)
-    return biggest_index
+        biggest_index += 24
+    if biggest_val < skip_koeff or w < HEIGHT / 3:
+        return (-1, -1)
+        # return (-1, -1, used_width)
+    # print(biggest_val, biggest_pos, biggest_index, w, h)
+    # used_width += w
+    cv2.rectangle(img, (biggest_pos[0] + int(w / 100), biggest_pos[1]), (biggest_pos[0] + w - int(w / 100), biggest_pos[1] + h), (0, 0, 255), -1)
+    return (biggest_index, biggest_pos[0])
 
 # url = input()
-url = "https://ucarecdn.com/f628d67d-377e-4d1e-867c-607ebf161ecc/"
-# url = "https://ucarecdn.com/f628d67d-377e-4d1e-867c-607ebf161ecc/"
 # url = "https://stepik.org/media/attachments/course/187016/corrected_templ.png"
-resp = requests.get(url, stream=True, timeout=100.0).raw
-image = np.asarray(bytearray(resp.read()), dtype="uint8")
+image = np.frombuffer(bytes([int(i, 16) for i in input().split()]), np.uint8)
+# fst = "https://ucarecdn.com/f628d67d-377e-4d1e-867c-607ebf161ecc/"
+# nigger = "https://stepik.org/media/attachments/course/187016/text_recogn_test_1.png"
+# Q_text = "https://stepik.org/media/attachments/course/187016/hpgQ.png"
+# full_test = "https://stepik.org/media/attachments/course/187016/Full_test.png"
+# e_full_test = "https://stepik.org/media/attachments/course/187016/extended_Full_test.png"
+# url = e_full_test
+# resp = requests.get(url, stream=True, timeout=100.0).raw
+# image = np.asarray(bytearray(resp.read()), dtype="uint8")
 image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+ret, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
 image = cut_corners(image)
 ih, iw = image.shape
 image = cv2.resize(image, (int(iw / ih * HEIGHT), HEIGHT))
+sh("SDf", image)
+# print(image.shape)
+ih, iw = image.shape
 # image_contours = get_text_contours(image)
+
+Q_url = "https://stepik.org/media/attachments/course/187016/Q.png"
+resp = requests.get(Q_url, stream=True, timeout=100.0).raw
+Q = np.asarray(bytearray(resp.read()), dtype="uint8")
+Q = cv2.imdecode(Q, cv2.IMREAD_GRAYSCALE)
+ret, Q = cv2.threshold(Q, 127, 255, cv2.THRESH_BINARY)
+Q = cut_corners(Q)
+Q_h, Q_w = Q.shape
+Q = cv2.resize(Q, (int(Q_w / Q_h * HEIGHT) - 1, HEIGHT - 1))
+Q_h, Q_w = Q.shape
+
+cleared_q_num = 0
+letters = []
+Q_min_val, Q_max_val, Q_min_loc, Q_max_loc = cv2.minMaxLoc(cv2.matchTemplate(image, Q, cv2.TM_CCOEFF_NORMED))
+ih, old_iw = image.shape
+while Q_max_val > 0.9:
+    cleared_q_num += 1
+    # sh("before remove Q", image, 0)
+    cv2.rectangle(image, (Q_max_loc[0] - 5, Q_max_loc[1]), (Q_max_loc[0] + Q_w + 5, Q_max_loc[1] + Q_h), (255, 255, 255), -1)
+    # sh("after remove Q", image)
+    letters.append(('Q', int(Q_max_loc[0])))
+    Q_min_val, Q_max_val, Q_min_loc, Q_max_loc = cv2.minMaxLoc(cv2.matchTemplate(image, Q, cv2.TM_CCOEFF_NORMED))
+# print(cleared_q_num)
+image = cut_corners(image)
+ih, iw = image.shape
+image = cv2.resize(image, (int(iw / ih * HEIGHT), HEIGHT))
+new_ih, new_iw = image.shape
+# print(new_iw)
+for i in range(len(letters)):
+    letters[i] = (letters[i][0], int(letters[i][1] * new_iw / old_iw))
 
 mn = "https://stepik.org/media/attachments/course/187016/MN_templ.png"
 templ = "https://stepik.org/media/attachments/course/187016/text_template.png"
 templ_corrected_MN = "https://stepik.org/media/attachments/course/187016/corrected_templ.png"
 with_no_q = "https://stepik.org/media/attachments/course/187016/alphabet_with_no_Q.png"
+noq = "https://stepik.org/media/attachments/course/187016/literally_no_Q.png"
 
-resp = requests.get(with_no_q, stream=True, timeout=100.0).raw
+resp = requests.get(noq, stream=True, timeout=100.0).raw
 template = np.asarray(bytearray(resp.read()), dtype="uint8")
 template = cv2.imdecode(template, cv2.IMREAD_GRAYSCALE)
+ret, template = cv2.threshold(template, 127, 255, cv2.THRESH_BINARY)
+template = cut_corners(template)
+templ_h, templ_w = template.shape
+# template = cv2.resize(template, (int(templ_w / templ_h * HEIGHT), HEIGHT))
 # print(template.shape)
 # cv2.imshow("alphabet", template)
-# wait()
-template = cut_corners(template)
-# cv2.imshow("alphabet", template)
-# wait()
-print(template.shape)
+#
+# print(template.shape)
 
 alph_letters = get_letters(template.copy())
 # cv2.imshow("letter A", alph_letters[0])
-wait()
+
 alph_txt = sort_by_text(alph_letters, template.copy())   #letters in alphabet order
 
 # for i in range(len(alph_txt)):
 #     print(i)
 #     cv2.imshow("Dfg", alph_txt[i])
-#     wait()
+#
 # for el in alph_txt:
 #     cv2.imshow("let", el)
-#     wait()
+#
 #     cv2.destroyAllWindows()
-letters = [()]
 while(True):
-    res = get_letter(image, alph_txt)
-    if res == -1:
+    # val, pos, used_width = get_letter(image, alph_txt, used_width)
+    val, pos = get_letter(image, alph_txt)
+    # if val == -1 or used_width > iw * 0.95:
+    if val == -1:
         break
-    letters.append(chr(res))
-    print(chr(res))
+    letters.append((chr(val), pos))
+    # print(chr(res))
+# print(letters)
+sorted_letters = sorted(letters, key=lambda x: x[1])
+# for el in sorted_letters:
+#     print("Chr", el[0], "at", el[1])
+for el in sorted_letters:
+    print(el[0], end='')
