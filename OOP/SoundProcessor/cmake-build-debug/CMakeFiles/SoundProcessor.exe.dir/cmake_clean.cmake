625beefcb1abcc95a6017165c1238c1946d483df
Gleb Kuznetsov <g.kuznetsov2@g.nsu.ru>
2023-11-28 10:46:02 +0700

    repair conflict

---
+++ OOP/SoundProcessor/cmake-build-debug/CMakeFiles/SoundProcessor.exe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SoundProcessor.exe.dir/main.cpp.obj"
+  "CMakeFiles/SoundProcessor.exe.dir/main.cpp.obj.d"
+  "SoundProcessor.exe.exe"
+  "SoundProcessor.exe.exe.manifest"
+  "SoundProcessor.exe.pdb"
+  "libSoundProcessor.exe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SoundProcessor.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OOP/SoundProcessor/cmake-build-debug/CMakeFiles/SoundProcessor.exe.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SoundProcessor.exe.
+# This may be replaced when dependencies are built.
